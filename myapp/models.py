853de8911fce9bc29006a5b903fcbff25e5e2dc5
jjlin133 <63435164+jjlin133@users.noreply.github.com>
2021-05-28 08:18:47 +0800

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class users(models.Model):
+    uid = models.CharField(max_length=50, null=False)
+    question = models.CharField(max_length=250, null=False)
+    
+    def __str__(self):
+        return self.uid
+
+# 連結至 db.sqlite3 之資料表 myapp_ntuhqna
+class ntuhqna(models.Model):
+    title = models.CharField(max_length=50,null=False)
+    que = models.CharField(max_length=128,null=False)
+    ans = models.CharField(max_length=250,null=False)
+    
+    def _str_(self):
+        return self.qid
